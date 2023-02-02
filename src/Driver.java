b6ed608b03da05096fbb3aa0ec36cc53d17da1f6
madrid7200 <106935787+madrid7200@users.noreply.github.com>
2023-02-01 22:19:20 -0600

    file reader

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Driver {
+	public static void main(String[] args) throws FileNotFoundException {
+		String archivo = "datos.txt";
+		String postfix = "";
+		
+		File file = new File(archivo);
+		Scanner sc = new Scanner(file);
+		
+	}
+
 
 }
